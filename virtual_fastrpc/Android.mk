1c18a76b0909dee2fb53d305e08fd1464720bb0e
Vinay Sudra <quic_vsudra@quicinc.com>
2023-06-07 11:22:06 +0530

    char: virtio_fastrpc: Initial commit for virtio-fastrpc driver
    Commit branch :  msm-5.15
    Last commit id : e735eff0d69612cce6787b3a98cfc7a71acc4d9b
    
    Change-Id: I1388e5759bd0e5f3ece69fb9bfd440d8ce759d75
    Signed-off-by: Vinay Sudra <quic_vsudra@quicinc.com>

---
+++ virtual_fastrpc/Android.mk
@@ -0,0 +1,38 @@
+FASTRPC_DLKM_ENABLED := true
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+	ifeq ($(TARGET_KERNEL_DLKM_FASTRPC_OVERRIDE), false)
+		FASTRPC_DLKM_ENABLED := false
+	endif
+endif
+
+ifeq ($(FASTRPC_DLKM_ENABLED), true)
+DLKM_DIR   := device/qcom/common/dlkm
+
+LOCAL_PATH := $(call my-dir)
+
+VRPC_BLD_DIR := $(abspath .)/vendor/qcom/opensource/platform-kernel/virtual_fastrpc
+
+include $(CLEAR_VARS)
+$(info DLKM_DIR = $(DLKM_DIR))
+LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+LOCAL_MODULE      := vfastrpc-module-symvers
+LOCAL_MODULE_STEM         := Module.symvers
+LOCAL_MODULE_KBUILD_NAME  := Module.symvers
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
+
+include $(CLEAR_VARS)
+$(info DLKM_DIR = $(DLKM_DIR))
+LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+LOCAL_MODULE      := vfastrpc.ko
+LOCAL_MODULE_KBUILD_NAME := vfastrpc.ko
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS += VRPC_ROOT=$(VRPC_BLD_DIR)
+KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
+
+# print out variables
+$(info KBUILD_OPTIONS = $(KBUILD_OPTIONS))
+$(info intermediates dsp symvers path = $(call intermediates-dir-for,DLKM,vfastrpc-module-symvers))
+$(info DLKM_DIR = $(DLKM_DIR))
+endif
