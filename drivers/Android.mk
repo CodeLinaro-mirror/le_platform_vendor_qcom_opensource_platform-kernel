f5b129e5a04a3ca7960df5b1e59a5a956fb8e77c
Abdul Salam <quic_asalam@quicinc.com>
2023-06-09 16:52:38 +0530

    platform-kernel: Move wallpower charger driver to techpack
    This driver is a snapshot from msm-5.15 branch at
    commit e229764c0cb7 ("drivers: power: add wallpower driver").
    
    Change-Id: I3ca919d600eed4f14610a2990946d78893cf73b4
    Signed-off-by: Abdul Salam <quic_asalam@quicinc.com>

--- drivers/Android.mk
+++ drivers/Android.mk
@@ -1,4 +1,5 @@
 AOP_SET_DDR_SELECT := CONFIG_QCOM_AOP_SET_DDR=m
+WALLPOWER_CHARGER_SELECT := CONFIG_WALLPOWER_CHARGER=m
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -19,6 +20,33 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOC
 KBUILD_OPTIONS := PLAT_DRV_ROOT=$(SOC_BLD_DIR)
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 
+# Drivers for both LA-Metal and LA-GVM
+##########################################################
+KBUILD_OPTIONS += MODNAME=wallpower-charger
+KBUILD_OPTIONS += $(WALLPOWER_CHARGER_SELECT)
+
+ifneq ($(TARGET_BOARD_AUTO),true)
+KBUILD_OPTIONS += KBUILD_EXTRA_SYMBOLS+=$(PWD)/$(call intermediates-dir-for,DLKM,wallpower_charger_select-module-symvers)/Module.symvers
+endif
+
+###########################################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+LOCAL_MODULE              := wallpower_charger.ko
+LOCAL_MODULE_KBUILD_NAME  := wallpower_charger.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+
+ifneq ($(TARGET_BOARD_AUTO),true)
+LOCAL_REQUIRED_MODULES    += wallpower_charger_select-module-symvers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(call intermediates-dir-for,DLKM,wallpower_charger_select-module-symvers)/Module.symvers
+endif
+
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
+
+# Drivers for only LA-Metal
+###########################################################
 ifneq (,$(filter $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_au))
 KBUILD_OPTIONS += MODNAME=aop_set_ddr_freq
 KBUILD_OPTIONS += $(AOP_SET_DDR_SELECT)
