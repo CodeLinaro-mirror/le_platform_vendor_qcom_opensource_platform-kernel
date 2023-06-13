794e876eefede1639e63984f781f64bf80fa2a6c
Abdul Salam <quic_asalam@quicinc.com>
2023-06-13 17:22:19 +0530

    platform-kernel: Move socinfo_dt driver to techpack
    This driver is a snapshot from msm-5.15 branch at
    commit caf6f2150dd5 ("soc: qcom: Add DT based socinfo driver").
    
    Change-Id: Ia7a9afcf4376a5c7ec529ac25b7bf1b2dc07d923
    Signed-off-by: Abdul Salam <quic_asalam@quicinc.com>

--- platform_kernel_product.mk
+++ platform_kernel_product.mk
@@ -1,11 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 ifeq ($(call is-board-platform-in-list,msmnile), true)
-ifeq (,$(filter $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_gvmq))
-    PRODUCT_PACKAGES += aop-set-ddr.ko
+# Drivers for both Metal and GVM
     PRODUCT_PACKAGES += wallpower_charger.ko
+
+ifeq (,$(filter $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_gvmq))
+# Drivers for Metal only
+    PRODUCT_PACKAGES += aop-set-ddr.ko
     PRODUCT_PACKAGES += silent_boot.ko
     PRODUCT_PACKAGES += silent-mode-hw-monitoring.ko
     PRODUCT_PACKAGES += dump_boot_log.ko
+
+else
+# Drivers for GVM only
+    PRODUCT_PACKAGES += socinfo_dt.ko
+
 endif
 endif
