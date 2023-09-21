7dc5047147cd024d3d7efb38dd9662744302fa0c
Vijayakumar Badiger <quic_vbadiger@quicinc.com>
2023-09-20 17:02:00 -0700

    platform-kernel: disable platform dlkm for hgy gvm
    Change-Id: I1863bdd241446603774a0fbefebb9f1e86570e13
    Signed-off-by: Vijayakumar Badiger <quic_vbadiger@quicinc.com>

--- platform_kernel_board.mk
+++ platform_kernel_board.mk
@@ -1,5 +1,5 @@
 #SPDX-License-Identifier: GPL-2.0-only
-
+ifneq ($(TARGET_USES_GY), true)
 ifeq ($(call is-board-platform-in-list,sdmsteppe msmnile gen4), true)
 # Drivers for both Metal and GVM
     BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/wallpower_charger.ko
@@ -47,5 +47,6 @@ ifeq (,$(filter $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), sm6150_gvmq))
     BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/adsp_vote_smp2p.ko
     BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/adsp_vote_smp2p.ko
 
+endif
 endif
 endif
