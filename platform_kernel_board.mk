1380246db513d33969c266da2e25ddbaf86aec35
Steven Veigas <quic_sveigas@quicinc.com>
2024-05-24 00:10:35 +0530

    platform-kernel: Support for Boot Markers
    Boot marker print the mpm timer value
    
    Change-Id: I0a39ab8cb59321afcf543191abf9905268efb6f0
    Signed-off-by: Steven Veigas <quic_sveigas@quicinc.com>

--- platform_kernel_board.mk
+++ platform_kernel_board.mk
@@ -4,6 +4,7 @@ ifeq ($(call is-board-platform-in-list, $(MSMSTEPPE) msmnile gen4), true)
     BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/wallpower_charger.ko
     BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/wallpower_charger.ko
     BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/wallpower_charger.ko
+    BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/boot_marker.ko
 
 ifeq (,$(filter msmnile_gvmq gen4_gvm gen4_hgy gen4_gvm_gy, $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)))
 # Drivers for Metal only
