eaf7abdf1e73ba5a328138679f8c90c7942dffc7
Anvesh Jain P <quic_ajainp@quicinc.com>
2024-09-11 17:24:30 +0530

    virtual_fastrpc: Fix use-after-free issue while accessing ctx data
    Fix use-after-free issue while accessing ctx data which is already
    freed in context_free. Accessing context after completing the message
    work might lead into use-after-free error.
    
    Change-Id: I44abedbc95655a08172046f37bd1787010138087
    Signed-off-by: Anvesh Jain P <quic_ajainp@quicinc.com>
    (cherry picked from commit a36a87b0afcdb94d7825fa1bd18a0888fb74842c)

--- drivers/virtual_fastrpc/dsp/virtio_fastrpc_base.c
+++ drivers/virtual_fastrpc/dsp/virtio_fastrpc_base.c
@@ -234,7 +234,7 @@ static const struct file_operations fops = {
 static int recv_single(struct virt_msg_hdr *rsp, unsigned int len)
 {
 	struct vfastrpc_apps *me = &gfa;
-	struct virt_fastrpc_msg *msg;
+	struct virt_fastrpc_msg *msg = NULL;
 
 	if (len != rsp->len) {
 		dev_err(me->dev, "msg %u len mismatch,expected %u but %d found\n",
@@ -243,23 +243,24 @@ static int recv_single(struct virt_msg_hdr *rsp, unsigned int len)
 	}
 	spin_lock(&me->msglock);
 	msg = me->msgtable[rsp->msgid];
-	spin_unlock(&me->msglock);
 
 	if (!msg) {
 		dev_err(me->dev, "msg %u already free in table[%u]\n",
 				rsp->cmd, rsp->msgid);
+		spin_unlock(&me->msglock);
 		return -EINVAL;
 	}
 	msg->rxbuf = (void *)rsp;
+
+	if (msg->ctx)
+		trace_recv_single_end(msg->ctx);
 
 	if (msg->ctx && msg->ctx->asyncjob.isasyncjob)
 		vfastrpc_queue_completed_async_job(msg->ctx);
 	else
 		complete(&msg->work);
 
-	if (msg->ctx)
-		trace_recv_single_end(msg->ctx);
-
+	spin_unlock(&me->msglock);
 	return 0;
 }
 
