5c19937c5c5db2d44e8b8ae4c6b221d345c56d94
Minghao Xue <quic_mingxue@quicinc.com>
2025-07-04 17:29:40 +0800

    platform-kernel: hybrid_fastrpc: enhance debugfs info
    Print out invoke handle info in debugfs.
    
    Change-Id: Id211548df94af62a6f914f9ee376f39db3c0b9b0
    Signed-off-by: Minghao Xue <quic_mingxue@quicinc.com>

--- drivers/virtual_fastrpc/dsp/fastrpc_core.c
+++ drivers/virtual_fastrpc/dsp/fastrpc_core.c
@@ -946,6 +946,7 @@ void print_ictx_info(struct seq_file *s_file, struct fastrpc_invoke_ctx *ictx)
 	seq_printf(s_file, "\n %s %10s %llu", "ctxid", ":", ictx->ctxid);
 	seq_printf(s_file, "\n %s %3s %d", "is_work_done", ":", ictx->is_work_done);
 	seq_printf(s_file, "\n %s %9s %llu", "msg_sz", ":", ictx->msg_sz);
+	seq_printf(s_file, "\n %s %9s 0x%x", "handle", ":", ictx->handle);
 }
 
 void print_ctx_info(struct seq_file *s_file, struct fastrpc_channel_ctx *ctx)
