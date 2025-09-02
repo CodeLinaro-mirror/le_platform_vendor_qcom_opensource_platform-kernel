6ed6e40c4efcaa7fc1b02229f0417bf9443f1905
Sai Kumar Kurapati <kurasaik@qti.qualcomm.com>
2025-09-02 02:58:45 -0700

    Add support for single tree lunch target
    Change-Id: I06bc2806d4e87180efbef510aaeec8a95f08b0e1

--- drivers/Android.mk
+++ drivers/Android.mk
@@ -221,7 +221,7 @@ endif
 
 # Drivers for LA-GVM only
 ###########################################################
-ifneq (,$(filter msmnile_gvmq gen4_gvm gen4_hgy gen5_gvm_gy gen5_gvm, $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)))
+ifneq (,$(filter msmnile_gvmq gen4_gvm gen4_hgy gen5_gvm_gy gen5_gvm gen5_gvm_sgt, $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)))
 KBUILD_OPTIONS += MODNAME=qcom-dt-socinfo
 KBUILD_OPTIONS += $(SOCINFO_DT_SELECT)
 
