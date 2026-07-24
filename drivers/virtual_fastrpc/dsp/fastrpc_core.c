f0130822f21b96c354edb1bd2c3fb8c37f68c8e1
Ping Jiang <pingj@qti.qualcomm.com>
2026-07-24 10:43:51 +0200

    platform-kernel: hybrid_fastrpc: Handle race-condition in dsp signal
    The `fastrpc_dspsignal_wait` function currently checks the
    signal state before waiting for a signal from the DSP. However,
    if the signal is already received before the check, it results
    in an infinite loop, causing excessive resource usage.
    
    This change addresses the race condition by checking both the
    pending and signaled states. If the signal is not in the pending
    state, it directly checks for the signaled state, resets the states,
    and returns to avoid looping.
    
    Change-Id: I4b51163284ebf5bd8f95d5a8430ba0e6b4e0cc4e
    Signed-off-by: Ping Jiang <pingj@qti.qualcomm.com>

--- drivers/virtual_fastrpc/dsp/fastrpc_core.c
+++ drivers/virtual_fastrpc/dsp/fastrpc_core.c
@@ -2102,6 +2102,12 @@ static int fastrpc_dspsignal_wait(struct fastrpc_user *fl,
 		if ((s->state == DSPSIGNAL_STATE_CANCELED) ||
 				(s->state == DSPSIGNAL_STATE_UNUSED))
 			err = -EINTR;
+		if (s->state == DSPSIGNAL_STATE_SIGNALED) {
+			/* Signal already received from DSP. Reset signal state and return */
+			s->state = DSPSIGNAL_STATE_PENDING;
+			reinit_completion(&s->comp);
+		}
+
 		spin_unlock_irqrestore(&fl->dspsignals_lock, irq_flags);
 		DSPSIGNAL_VERBOSE("signal %u in state %u, complete wait immediately",
 				signal_id, s->state);
